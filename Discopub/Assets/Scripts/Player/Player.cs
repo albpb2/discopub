2c491d636994782f4035311eb88cf957a2b81281
albpb2 <albpb2@gmail.com>
2019-07-27 19:28:11 +0200

    Create player command sender to be used by ui elements.

---
+++ Discopub/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using System.Linq;
+
+namespace Assets.Scripts.Player
+{
+    public class Player : CaptainsMessPlayer
+    {
+        private TextMeshProUGUI _textSentLabel;
+        private TMP_InputField _inputField;
+        private CaptainsMess _captainMess;
+
+        [ClientRpc]
+        public void RpcNotifyMessage(string peerId, string message)
+        {
+            Debug.Log($"Player {peerId} sent {message}");
+            _textSentLabel.text = $"Player {peerId} sent {message}";
+        }
+
+        [Command]
+        public void CmdSendMessage()
+        {
+            Debug.Log($"Sending message {_inputField.text}");
+            foreach (var player in _captainMess.Players().Cast<Player>())
+            {
+                if (player.peerId != peerId)
+                {
+                    player.RpcNotifyMessage(peerId, _inputField.text);
+                }
+            }
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            var playerCommandSender = FindObjectOfType<PlayerCommandSender>();
+            playerCommandSender.SetPlayer(this);
+        }
+
+        protected void Start()
+        {
+            _textSentLabel = GameObject.FindGameObjectWithTag(Labels.UiElements.TextSentLabel).GetComponent<TextMeshProUGUI>();
+            _inputField = GameObject.FindGameObjectWithTag(Labels.UiElements.Textbox).GetComponent<TMP_InputField>();
+            _captainMess = GameObject.FindObjectOfType<CaptainsMess>();
+        }
+    }
+}
---
+++ Discopub/Assets/Scripts/Player/PlayerCommandSender.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerCommandSender : MonoBehaviour
+    {
+        private Player _player;
+
+        public void SetPlayer(Player player)
+        {
+            _player = player;
+        }
+
+        public void SendMessage() => _player.CmdSendMessage();
+    }
+}
