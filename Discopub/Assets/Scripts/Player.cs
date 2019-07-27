181a384f861eee5de88cef2025f1432b76757591
albpb2 <albpb2@gmail.com>
2019-07-27 18:43:46 +0200

    Create Player script with function to notify message.

---
+++ Discopub/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+namespace Assets.Scripts
+{
+    public class Player : CaptainsMessPlayer
+    {
+        TextMeshProUGUI textSentLabel;
+
+        [ClientRpc]
+        public void RpcNotifyMessage(string peerId, string message)
+        {
+            textSentLabel.text = $"Player {peerId} sent {message}";
+        }
+
+        protected void Start()
+        {
+            textSentLabel = GameObject.FindGameObjectWithTag("TextSentLabel").GetComponent<TextMeshProUGUI>();
+        }
+    }
+}
