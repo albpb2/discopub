308f5f45f6763b08025cc46f892e76aa920c3159
albpb2 <albpb2@gmail.com>
2019-07-28 19:07:44 +0200

    Create lobby manager to autoconnect on start.

---
+++ Discopub/Assets/Scripts/Scenes/LobbyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes
+{
+    public class LobbyManager : MonoBehaviour
+    {
+        [SerializeField]
+        private CaptainsMess _captainsMess;
+
+        public void Awake()
+        {
+            StartCoroutine(AutoConnect());
+        }
+
+        public IEnumerator AutoConnect()
+        {
+            const int secondsToWaitToAutoconnect = 1; // To allow for the captainsMess object to be active
+            yield return new WaitForSeconds(secondsToWaitToAutoconnect);
+            _captainsMess.AutoConnect();
+        }
+    }
+}
