df96d54536117fede7c4c17df1442664cf6f3643
albpb2 <albpb2@gmail.com>
2019-07-28 18:48:08 +0200

    Remove unused fields from player.

--- Discopub/Assets/Scripts/Player/Player.cs
+++ Discopub/Assets/Scripts/Player/Player.cs
@@ -1,27 +1,14 @@
-﻿using UnityEngine;
-using TMPro;
-
-namespace Assets.Scripts.Player
+﻿namespace Assets.Scripts.Player
 {
     public class Player : CaptainsMessPlayer
     {
-        private TextMeshProUGUI _textSentLabel;
-        private TMP_InputField _inputField;
-        private CaptainsMess _captainMess;
-
         public override void OnStartLocalPlayer()
         {
             base.OnStartLocalPlayer();
 
             var playerCommandSender = FindObjectOfType<PlayerCommandSender>();
             playerCommandSender.SetPlayer(this);
-        }
 
-        protected void Start()
-        {
-            _textSentLabel = GameObject.FindGameObjectWithTag(Labels.UiElements.TextSentLabel).GetComponent<TextMeshProUGUI>();
-            _inputField = GameObject.FindGameObjectWithTag(Labels.UiElements.Textbox).GetComponent<TMP_InputField>();
-            _captainMess = GameObject.FindObjectOfType<CaptainsMess>();
         }
     }
 }
