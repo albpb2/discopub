9a8a9f3d2866f4828a58adcb1b6017b95b6b2705
albpb2 <albpb2@gmail.com>
2019-08-07 17:36:15 +0200

    Create script to send pressed button's action to server.

---
+++ Discopub/Assets/Scripts/Buttons/ActionButtonController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Buttons
+{
+    public class ActionButtonController : MonoBehaviour
+    {
+        private string _actionName;
+        private Player.Player _player;
+
+        public void SetUp(string actionName, string playerPeerId)
+        {
+            _actionName = actionName;
+
+            var captainsMessNetworkManager = CaptainsMessNetworkManager.singleton as CaptainsMessNetworkManager;
+            _player = captainsMessNetworkManager.LobbyPlayers().Single(p => p.peerId == playerPeerId) as Player.Player;
+        }
+
+        public void SubmitAction()
+        {
+            _player.CmdSubmitAction(_actionName);
+        }
+    }
+}
