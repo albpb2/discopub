e4c4025267108af49fb0e312022ea31b5bfc900a
albpb2 <albpb2@gmail.com>
2019-08-07 17:04:59 +0200

    Create class to resolve whether an action is right or not and update points.

---
+++ Discopub/Assets/Scripts/Game/ActionDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class ActionDispatcher : MonoBehaviour
+    {
+        private const int PointsToWinPerRightAction = 10;
+        private const int PointsToLosePerWrongAction = 5;
+
+        [SerializeField]
+        private PlayerActionsManager _playerActionsManager;
+        [SerializeField]
+        private MatchPointsCounter _matchPointsCounter;
+
+        public void DispatchAction(string actionName)
+        {
+            if (_playerActionsManager.IsRightAction(actionName))
+            {
+                _matchPointsCounter.IncreasePoints(PointsToWinPerRightAction);
+            }
+            else
+            {
+                _matchPointsCounter.DecreasePoints(PointsToLosePerWrongAction);
+            }
+        }
+    }
+}
