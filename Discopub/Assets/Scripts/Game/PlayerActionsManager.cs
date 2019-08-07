6403046ae1de8e99976d63e13e150acf24ddd6e0
albpb2 <albpb2@gmail.com>
2019-08-07 16:48:11 +0200

    Create script to manage the player actions.

---
+++ Discopub/Assets/Scripts/Game/PlayerActionsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class PlayerActionsManager : MonoBehaviour
+    {
+        private Dictionary<string, List<string>> _actionsPerPlayer;
+
+        public void InitializeActions(List<CaptainsMessPlayer> players)
+        {
+            _actionsPerPlayer = new Dictionary<string, List<string>>();
+
+            var i = 0;
+
+            foreach (var player in players)
+            {
+                _actionsPerPlayer.Add(player.peerId, new List<string>());
+                _actionsPerPlayer[player.peerId].Add($"Player {i} test target rpc 1");
+                _actionsPerPlayer[player.peerId].Add($"Player {i} test target rpc 2");
+                _actionsPerPlayer[player.peerId].Add($"Player {i} test target rpc 3");
+                i++;
+            }
+        }
+    }
+}
