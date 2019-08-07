82f5e84018e5e2ead8c265908ed4b64a5d2b8fc1
albpb2 <albpb2@gmail.com>
2019-08-07 16:47:43 +0200

    Create script to manage game points.

---
+++ Discopub/Assets/Scripts/Game/MatchPointsCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Game
+{
+    public class MatchPointsCounter : NetworkBehaviour
+    {
+        [SerializeField]
+        private TMPro.TMP_Text _matchPointsText;
+
+        private int _currentPoints;
+        private int _maxPoints;
+        
+        public void ResetPoints()
+        {
+            _currentPoints = 0;
+            PrintPoints();
+        }
+
+        public void SetMaxPoints(int maxPoints)
+        {
+            _maxPoints = maxPoints;
+            PrintPoints();
+        }
+
+        public void SetPoints(int currentPoints)
+        {
+            _currentPoints = Math.Min(currentPoints, _maxPoints);
+            PrintPoints();
+        }
+
+        public void IncreasePoints(int pointsToAdd)
+        {
+            _currentPoints += pointsToAdd;
+            _currentPoints = Math.Min(_currentPoints, _maxPoints);
+            PrintPoints();
+        }
+
+        private void PrintPoints()
+        {
+            _matchPointsText.text = $"{_currentPoints}/{_maxPoints}";
+        }
+    }
+}
