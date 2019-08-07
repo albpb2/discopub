188849cfcabb044a35f0eb696f035371b23aeb77
albpb2 <albpb2@gmail.com>
2019-08-07 16:48:36 +0200

    Create script to disable server objects on client.

---
+++ Discopub/Assets/Scripts/Network/ServerObjectsManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Network
+{
+    /// <summary>
+    /// This component will disable its object and its children if the player is not
+    /// the server.
+    /// </summary>
+    public class ServerObjectsManager : NetworkBehaviour
+    {
+        protected void Awake()
+        {
+            if (!isServer)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
