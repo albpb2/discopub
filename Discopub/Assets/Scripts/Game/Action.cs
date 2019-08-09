028db17d2bc34d98c89c86eef6635836561ee134
albpb2 <albpb2@gmail.com>
2019-08-09 17:25:05 +0200

    Create action class.

---
+++ Discopub/Assets/Scripts/Game/Action.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Game
+{
+    public class Action
+    {
+        public string Name { get; set; }
+        public string ControlType { get; set; }
+        public string[] Values { get; set; }
+        public int ActionPoints { get; set; }
+    }
+}
