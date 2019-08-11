21f13b9e48c7b5c409ddd24ec33a1a4009a62a7f
albpb2 <albpb2@gmail.com>
2019-08-11 10:56:46 +0200

    Create game difficulty class.

---
+++ Discopub/Assets/Scripts/Game/GameDifficulty.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Game
+{
+    public class GameDifficulty
+    {
+        public int MinRound { get; set; }
+
+        public int MaxRound { get; set; }
+
+        public int ActionPoints { get; set; }
+
+        public int ActionSeconds { get; set; }
+    }
+}
