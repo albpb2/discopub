11ef1170fa14a796bd099d48591c2ee3aa4da689
albpb2 <albpb2@gmail.com>
2019-08-11 17:29:04 +0200

    Remove scaffolding from match manager.

--- Discopub/Assets/Scripts/Game/MatchManager.cs
+++ Discopub/Assets/Scripts/Game/MatchManager.cs
@@ -1,7 +1,4 @@
-﻿using Assets.Scripts.Buttons;
-using Assets.Scripts.Importers;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -10,76 +7,27 @@ namespace Assets.Scripts.Game
     public class MatchManager : NetworkBehaviour
     {
         [SerializeField]
-        private Timer _timer;
-        [SerializeField]
-        private GameObject _actionCountdownPrefab;
-        [SerializeField]
-        private GameObject _networkedObjectsRoot;
-        [SerializeField]
         private ProgressBar _actionProgressBar;
         [SerializeField]
         private MatchPointsCounter _matchPointsCounter;
         [SerializeField]
-        private ActionButtonsPanelCreator _actionButtonsPanelCreator;
+        private RoundManager _roundManager;
 
-        private List<CaptainsMessPlayer> _players;
-        private List<ActionCountdown> _actionCountdowns;
-        private Dictionary<string, List<string>> _actionsPerPlayer;
-
-        private IEnumerator StartTimer()
+        private IEnumerator StartFirstRound()
         {
-            const int secondsToStartTimer = 3;
-            yield return new WaitForSeconds(secondsToStartTimer);
-
-            Debug.Log("Starting timer from match manager");
-            _timer.StartTimer();
-
-            //_buttonPanelManager.CreatePanel();
-
-            var actions = ActionImporter.ImportActions("Config/Actions", true).ToArray();
-            foreach(var player in _players)
-            {
-                _actionButtonsPanelCreator.TargetCreateActionButtonsPanels(player.connectionToClient, actions);
-            }
-
-            var goals = GoalImporter.ImportGoals("Config/Goals", true, 2).ToArray();
-
-            var difficultyLevels = GameDifficultyImporter.ImportGamedifficultyLevels("Config/DifficultyLevels", true).ToArray();
+            const int secondsToStartFirstRound = 3;
+            yield return new WaitForSeconds(secondsToStartFirstRound);
         }
 
         protected void Start()
         {
-            const int timerSeconds = 120;
-            _timer.SetTime(timerSeconds);
-
-            StartCoroutine(StartTimer());
-
-            _players = (CaptainsMessNetworkManager.singleton as CaptainsMessNetworkManager).LobbyPlayers();
+            StartCoroutine(StartFirstRound());
 
             if (isServer)
             {
-                StartCountdowns();
+                _roundManager.SetUpRound(1);
                 const int maxPoints = 200;
                 _matchPointsCounter.SetMaxPoints(maxPoints);
-            }
-        }
-
-        private void StartCountdowns()
-        {
-            _actionCountdowns = new List<ActionCountdown>();
-            foreach (var player in _players)
-            {
-                Debug.Log($"Starting countdown for player {player.peerId}");
-                ClientScene.RegisterPrefab(_actionCountdownPrefab);
-                var actionCountdownGameObject = Instantiate(_actionCountdownPrefab);
-                NetworkServer.Spawn(actionCountdownGameObject);
-                actionCountdownGameObject.transform.parent = _networkedObjectsRoot.transform;
-
-                var actionCountdown = actionCountdownGameObject.GetComponent<ActionCountdown>();
-                _actionCountdowns.Add(actionCountdown);
-                actionCountdown.AssignToPlayer(player);
-                actionCountdown.SetTotalTime(Random.Range(5, 15));
-                actionCountdown.StartCountdown();
             }
         }
     }
