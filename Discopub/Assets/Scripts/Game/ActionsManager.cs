a35319090cb15559076f717e60ba0985d71dadc6
albpb2 <albpb2@gmail.com>
2019-09-01 16:04:00 +0200

    Missing files.

---
+++ Discopub/Assets/Scripts/Game/ActionsManager.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Extensions;
+using Assets.Scripts.Importers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.Game
+{
+    public class ActionsManager
+    {
+        private List<Action> _actions;
+
+        public ActionsManager()
+        {
+            ImportActions();
+        }
+
+        public List<Action> GetShuffledActionsList()
+        {
+            var serializedActions = JsonConvert.SerializeObject(_actions);
+            var clonedActions = JsonConvert.DeserializeObject<List<Action>>(serializedActions);
+            clonedActions.Shuffle();
+
+            return clonedActions;
+        }
+
+        private void ImportActions()
+        {
+            _actions = ActionImporter.ImportActions("Config/Actions", true).ToList();
+        }
+    }
+}
