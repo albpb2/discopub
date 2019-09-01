7414fbd524aead9d85fdb2117b3ed30fe6d57876
albpb2 <albpb2@gmail.com>
2019-09-01 16:55:39 +0200

    Create controller for on/off button.

---
+++ Discopub/Assets/Scripts/Buttons/OnOfButtonController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Buttons
+{
+    public class OnOfButtonController : MonoBehaviour
+    {
+        private bool _enabled = false;
+
+        public const string OnValue = "ON";
+        public const string OffValue = "OFF";
+
+        private string _actionName;
+        private Player.Player _player;
+
+        public void SetUp(string actionName, string actionText, string playerPeerId)
+        {
+            _actionName = actionName;
+
+            var captainsMessNetworkManager = CaptainsMessNetworkManager.singleton as CaptainsMessNetworkManager;
+            _player = captainsMessNetworkManager.LobbyPlayers().Single(p => p.peerId == playerPeerId) as Player.Player;
+
+            var buttonText = GetComponentInChildren<Text>();
+            buttonText.text = actionText;
+        }
+
+        public void SetOn()
+        {
+            if (_enabled)
+            {
+                return;
+            }
+
+            _player.CmdSubmitAction(_actionName, OnValue);
+        }
+
+        public void SetOff()
+        {
+            if (!_enabled)
+            {
+                return;
+            }
+
+            _player.CmdSubmitAction(_actionName, OffValue);
+        }
+    }
+}
