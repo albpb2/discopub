caa3114e43495017d246cfa413978536551c8bd9
albpb2 <albpb2@gmail.com>
2019-09-15 11:52:28 +0200

    Create drink button controller and prefab.

---
+++ Discopub/Assets/Scripts/Buttons/DrinkButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Buttons
+{
+    public class DrinkButtonController : MonoBehaviour
+    {
+        private const string DefaultDrinkButtonActionName = "";
+
+        private string _actionName;
+        private Player.Player _player;
+
+        public void SetUp(string actionName, string actionText, string playerPeerId)
+        {
+            _actionName = actionName;
+
+            var captainsMessNetworkManager = CaptainsMessNetworkManager.singleton as CaptainsMessNetworkManager;
+            _player = captainsMessNetworkManager.LobbyPlayers().Single(p => p.peerId == playerPeerId) as Player.Player;
+
+            var buttonText = GetComponentInChildren<Text>();
+            buttonText.text = actionText;
+        }
+
+        public void SubmitAction()
+        {
+            _player.CmdSubmitAction(_actionName, DefaultDrinkButtonActionName);
+        }
+    }
+}
