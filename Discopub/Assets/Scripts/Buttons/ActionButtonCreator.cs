450604404462002c7323c73a9af7b560ae966542
albpb2 <albpb2@gmail.com>
2019-09-21 18:21:03 +0200

    Create action button color manager.

--- Discopub/Assets/Scripts/Buttons/ActionButtonCreator.cs
+++ Discopub/Assets/Scripts/Buttons/ActionButtonCreator.cs
@@ -1,18 +1,36 @@
 ﻿using Assets.Scripts.Game;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Buttons
 {
     public class ActionButtonCreator : ButtonCreator
     {
+        private readonly ButtonColorManager _buttonColorManager;
+
         public ActionButtonCreator(ButtonInstantiator buttonInstantiator, GameObject actionButtonPrefab) : base(buttonInstantiator, actionButtonPrefab)
         {
+            _buttonColorManager = GameObject.FindObjectOfType<ButtonColorManager>();
         }
 
         protected override void SetUpButton(GameObject button, Action action, string playerPeerId)
         {
             var controller = button.GetComponentInChildren<ActionButtonController>();
             controller.SetUp(action.Name, action.Values[0], playerPeerId);
+
+            ChangeButtonColor(button);
+        }
+
+        private void ChangeButtonColor(GameObject button)
+        {
+            var buttonComponent = button.GetComponent<Button>();
+            buttonComponent.colors = _buttonColorManager.GetRandomColorBlock();
+            
+            if (_buttonColorManager.UseButtonColorForText)
+            {
+                var textComponent = button.GetComponentInChildren<Text>();
+                textComponent.color = buttonComponent.colors.normalColor;
+            }
         }
     }
 }
---
+++ Discopub/Assets/Scripts/Buttons/ButtonColorManager.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Extensions;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Buttons
+{
+    public class ButtonColorManager : MonoBehaviour
+    {
+        [SerializeField]
+        private List<ColorBlock> _actionButtonColorBlocks;
+        [SerializeField]
+        private bool _useButtonColorForText;
+
+        public ColorBlock GetRandomColorBlock() => _actionButtonColorBlocks.SelectRandomValue();
+        public bool UseButtonColorForText => _useButtonColorForText;
+    }
+}
